66298612634e9cad3aa115e719c9f0df1d0dac8c
dazealot88 <mathurinachiri@gmail.com>
2024-05-22 10:47:41 -0700

    edit bucket name

--- bucket.tf
+++ bucket.tf
@@ -5,22 +5,3 @@ resource "aws_s3_bucket" "lord-llingtong" {
     bucket = "lord-clingtong"
   
 }
-
-//adding bucket policy
-
-resource "aws_s3_bucket_policy" "demo-policy" {
-    bucket = "lord-clingtong"
-
-    policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                Effect = "Allow"
-                Principal = "*"
-                Action = "s3:GetObject",
-                Resource = "*"
-            }
-        ]
-    })
-  
-}
